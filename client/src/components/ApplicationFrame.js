0a75d957529f8308438f11a4735995acd26a56e1
annatsu <brendonpagano@gmail.com>
2020-01-12 20:42:05 -0300

    Create some components

---
+++ client/src/components/ApplicationFrame.js
@@ -0,0 +1,11 @@
+// React
+import React from 'react';
+import styled from '@emotion/styled';
+
+const ApplicationFrameContainer = styled.div``;
+
+const ApplicationFrame = (props) => {
+  return <ApplicationFrameContainer {...props} />;
+};
+
+export default ApplicationFrame;
---
+++ client/src/components/PrivateRoute.js
@@ -0,0 +1,29 @@
+// React
+import React, { useContext } from 'react';
+import PropTypes from 'prop-types';
+
+// Components
+import { Route, Redirect } from 'react-router-dom';
+
+// Contexts
+import AuthenticationContext from '../contexts/Authentication';
+
+const PrivateRoute = ({ component: Component, redirectToUrl = '/signin', ...rest }) => {
+  const { authenticated } = useContext(AuthenticationContext);
+
+  return (
+    <Route
+      {...rest}
+      render={({ location }) =>
+        authenticated ? <Component /> : <Redirect to={{ pathname: redirectToUrl, state: { from: location } }} />
+      }
+    />
+  );
+};
+
+PrivateRoute.propTypes = {
+  component: PropTypes.element,
+  redirectToUrl: PropTypes.string,
+};
+
+export default PrivateRoute;
