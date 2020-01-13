3c020261fb269d0bfe5bb792bf6bfb4768a26e91
annatsu <brendonpagano@gmail.com>
2020-01-12 22:05:14 -0300

    Add Bulma into the project

--- client/src/index.js
+++ client/src/index.js
@@ -5,4 +5,7 @@ import ReactDOM from 'react-dom';
 // Components
 import App from './App';
 
+// Stylesheets
+import 'bulma/css/bulma.css';
+
 ReactDOM.render(<App />, document.getElementById('root'));
