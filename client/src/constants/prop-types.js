57f2822c7ad96173bcbf226dc9eae0e99e7402be
annatsu <brendonpagano@gmail.com>
2020-01-13 05:43:43 -0300

    Create custom feedback prop validator

--- client/src/constants/prop-types.js
+++ client/src/constants/prop-types.js
@@ -9,3 +9,11 @@ export const collaboratorPropType = PropTypes.shape({
   company: PropTypes.string.isRequired,
   role: PropTypes.string.isRequired,
 });
+
+export const feedbackPropType = PropTypes.shape({
+  id: PropTypes.string.isRequired,
+  collaboratorId: PropTypes.string.isRequired,
+  createdAt: PropTypes.string.isRequired,
+  message: PropTypes.string.isRequired,
+  like: PropTypes.number.isRequired,
+});
