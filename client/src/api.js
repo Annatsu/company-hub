4ff7038fe4aca4bb384bf94473489202204fcad4
annatsu <brendonpagano@gmail.com>
2020-01-12 21:19:24 -0300

    Create axios api configuration instance

---
+++ client/src/api.js
@@ -0,0 +1,9 @@
+// Node Modules
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'http://5d8b64ad3c0aaf0014342c2a.mockapi.io/api/v1/',
+  timeout: 1000,
+});
+
+export default api;
