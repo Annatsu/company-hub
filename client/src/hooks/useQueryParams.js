84b46147de1a1feb938ce9191a1d6f41b6a7b31f
annatsu <brendonpagano@gmail.com>
2020-01-13 02:16:11 -0300

    Create custom hook for parsing query params on the URL

---
+++ client/src/hooks/useQueryParams.js
@@ -0,0 +1,9 @@
+// React
+import { useLocation } from 'react-router-dom';
+
+const useQueryParams = () => {
+  const location = useLocation();
+  return new URLSearchParams(location.search);
+};
+
+export default useQueryParams;
