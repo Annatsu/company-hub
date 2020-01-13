0f535734bf83ea4fc9edfddeeb5b41b7cb4a1907
annatsu <brendonpagano@gmail.com>
2020-01-13 00:27:37 -0300

    Add Font Awesome icons dependency

--- client/src/index.js
+++ client/src/index.js
@@ -7,5 +7,7 @@ import App from './App';
 
 // Stylesheets
 import 'bulma/css/bulma.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+import '@fortawesome/fontawesome-free/js/all';
 
 ReactDOM.render(<App />, document.getElementById('root'));
