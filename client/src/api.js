936fc02ca7a28dcc28431f9a12798d7b8d277715
annatsu <brendonpagano@gmail.com>
2020-01-13 06:54:06 -0300

    Increase API calls timeout tolerance

--- client/src/api.js
+++ client/src/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: 'http://5d8b64ad3c0aaf0014342c2a.mockapi.io/api/v1/',
-  timeout: 1000,
+  timeout: 3000,
 });
 
 export default api;
