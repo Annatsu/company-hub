7723a9cac9c889ab715551cccacbecb75779cc62
annatsu <brendonpagano@gmail.com>
2020-01-13 03:32:55 -0300

    Add CollaboratorDetails header

--- client/src/pages/CollaboratorDetails.js
+++ client/src/pages/CollaboratorDetails.js
@@ -1,10 +1,50 @@
 // React
 import React from 'react';
+import styled from '@emotion/styled';
+import { useIntl } from 'react-intl';
 
 // Prop Types
 import { collaboratorPropType } from '../constants/prop-types';
 
-const CollaboratorDetails = ({ collaborator }) => <div />;
+// Components
+import ProfilePicture from '../components/ProfilePicture';
+
+const Header = styled.header`
+  text-align: center;
+  & > h1,
+  & > p {
+    margin-top: 20px;
+  }
+
+  & > * {
+    margin-left: auto;
+    margin-right: auto;
+  }
+`;
+
+const CollaboratorDetails = ({ collaborator }) => {
+  const { formatMessage, formatDate } = useIntl();
+
+  return (
+    <>
+      <Header>
+        <ProfilePicture lg src={collaborator.avatar} />
+        <h1 className='title'>{formatMessage({ id: 'collaborator.greetings' })}</h1>
+        <p className='subtitle'>
+          {formatMessage(
+            { id: 'collaborator.introduction' },
+            {
+              name: <strong>{collaborator.name}</strong>,
+              role: <strong>{collaborator.role}</strong>,
+              company: <strong>{collaborator.company}</strong>,
+              date: <strong>{formatDate(collaborator.createdAt)}</strong>,
+            },
+          )}
+        </p>
+      </Header>
+    </>
+  );
+};
 
 CollaboratorDetails.propTypes = {
   collaborator: collaboratorPropType.isRequired,
