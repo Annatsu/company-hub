ee096ad2bb902fb5b85dfc3b1a3173a94ed43363
annatsu <brendonpagano@gmail.com>
2020-01-13 06:13:27 -0300

    Make 404 page a bit more friendly

--- client/src/pages/404.js
+++ client/src/pages/404.js
@@ -1,6 +1,16 @@
 // React
 import React from 'react';
+import { useIntl } from 'react-intl';
 
-const PageNotFound = () => <div>404 - Not Found</div>;
+const PageNotFound = () => {
+  const { formatMessage } = useIntl();
+
+  return (
+    <>
+      <h1 className='title'>{formatMessage({ id: 'routes.pageNotFound.title' })}</h1>
+      <h2 className='subtitle'>{formatMessage({ id: 'routes.pageNotFound.subtitle' })}</h2>
+    </>
+  );
+};
 
 export default PageNotFound;
