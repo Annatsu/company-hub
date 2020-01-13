6f598b138c2e03f43b69a50945e8839660960a4e
annatsu <brendonpagano@gmail.com>
2020-01-13 00:28:23 -0300

    Add SearchBar component

---
+++ client/src/components/SearchBar.js
@@ -0,0 +1,35 @@
+// React
+import React, { useCallback } from 'react';
+import PropTypes from 'prop-types';
+import { useIntl } from 'react-intl';
+
+const SearchBar = ({ onChange }) => {
+  const { formatMessage } = useIntl();
+
+  const onInputChange = useCallback((event) => {
+    const inputValue = event.target.value;
+    onChange(inputValue);
+  }, []);
+
+  return (
+    <div className='field'>
+      <div className='control has-icons-right'>
+        <input
+          className='input is-rounded'
+          type='text'
+          placeholder={formatMessage({ id: 'searchbar.placeholder' })}
+          onChange={onInputChange}
+        />
+        <span className='icon is-small is-right'>
+          <i className='fas fa-search'></i>
+        </span>
+      </div>
+    </div>
+  );
+};
+
+SearchBar.propTypes = {
+  onChange: PropTypes.func.isRequired,
+};
+
+export default SearchBar;
