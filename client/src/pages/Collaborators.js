febef862abea234ed52144a6da76c3f2b39d0d22
annatsu <brendonpagano@gmail.com>
2020-01-12 21:58:52 -0300

    Create blank LandingPage and Collaborators page

---
+++ client/src/pages/Collaborators.js
@@ -0,0 +1,6 @@
+// React
+import React from 'react';
+
+const CollaboratorsPage = () => <div />;
+
+export default CollaboratorsPage;
---
+++ client/src/pages/LandingPage.js
@@ -0,0 +1,10 @@
+// React
+import React from 'react';
+
+const LandingPage = () => (
+  <div>
+    <h1>Welcome to Company Hub!</h1>
+  </div>
+);
+
+export default LandingPage;
